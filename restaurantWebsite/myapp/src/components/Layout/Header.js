a932de93a9b158115dd8c95e71bfabbbeead3693
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-11-08 19:00:37 +0530

    header of restaurant app

---
+++ restaurantWebsite/myapp/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import mealImg from '../../assets/meals.jpg'
+
+function Header() {
+  return (
+    <React.Fragment>
+        <nav className={classes.navbar}>
+            <h2 className='reactMeals'>ReactMeals</h2>
+            <div className={classes.cartIcon}> <h4 className={classes.h4}> <FontAwesomeIcon icon={faCartShopping} /> Your Cart   <span> 0 </span> </h4></div>
+        </nav>
+        <div >
+            <img src={mealImg} alt="" className={classes.mealImg}  />
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Header
