11537960add5fec96c541ddbadc98c3c0dcf7948
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-11-09 06:39:46 +0530

    restaurant summary

---
+++ restaurantWebsite/myapp/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MealsSummary from './MealsSummary'
+
+function Meals() {
+  return (
+    <React.Fragment>
+        <MealsSummary />
+    </React.Fragment>
+  )
+}
+
+export default Meals
